adb1d7f2c8b568ce40249e7f4d4bcec16d89646c
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2018-12-13 11:36:59 -0600

    ethereum

--- imports/contracts/ownable.sol
+++ imports/contracts/ownable.sol
@@ -1,72 +1,90 @@
 pragma solidity ^0.4.17;
 
-/**
- * @title Ownable
- * @dev The Ownable contract has an owner address, and provides basic authorization control
- * functions, this simplifies the implementation of "user permissions".
- */
-contract Ownable {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-     * account.
-     */
-    constructor () internal {
-        _owner = msg.sender;
-        emit OwnershipTransferred(address(0), _owner);
+contract CardFactory {
+    address[] public deployedCards;
+    
+    function createCard(uint minimum) public {
+        address newCard = new Campaign(minimum, msg.sender);
+        deployedCards.push(newCard);
     }
-
-    /**
-     * @return the address of the owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
+    
+    function getDeployedCards() public view returns(address[]) {
+        return deployedCards;
     }
+}
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner());
+contract Card {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+    
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+    
+    modifier restricted() {
+        require(msg.sender == manager, "You are not the manager.");
         _;
     }
-
-    /**
-     * @return true if `msg.sender` is the owner of the contract.
-     */
-    function isOwner() public view returns (bool) {
-        return msg.sender == _owner;
+    
+    constructor(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+    
+    function contribute() public payable {
+        require(msg.value > minimumContribution, "You did not meet the minimum contribution value.");
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
+        });
+        requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        require(approvers[msg.sender], "You are not an approver.");
+        require(!request.approvals[msg.sender], "You have already voted.");
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+    
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(!request.complete, "Request is already completed.");
+        require(request.approvalCount > (approversCount / 2), "Less than half of your contributors have approved your request.");
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 
-    /**
-     * @dev Allows the current owner to relinquish control of the contract.
-     * @notice Renouncing to ownership will leave the contract without an owner.
-     * It will not be possible to call the functions with the `onlyOwner`
-     * modifier anymore.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
+    function getSummary() public view returns (
+        uint, uint, uint, uint, address
+    ) {
+        return (
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
     }
 
-    /**
-     * @dev Allows the current owner to transfer control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers control of the contract to a newOwner.
-     * @param newOwner The address to transfer ownership to.
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0));
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
