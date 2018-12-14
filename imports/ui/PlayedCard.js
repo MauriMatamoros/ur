9c4bbc44189d95db384399942236cbea4fa52b14
Isaias Valle <guitartsword@gmail.com>
2018-12-14 09:46:08 -0600

    show card

--- imports/ui/PlayedCard.js
+++ imports/ui/PlayedCard.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Grid, Card } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
 export default class Hand extends React.Component {
     state = {
-        cardCount: 3
+        palyerOneCard: '',
+        palyerTwoCard: '',
+    }
+    componentDidMount(){
+        Meteor.call('games.playerNum', (err, res)=>{
+            if(res === 1){
+                this.setState({
+                    palyerOneCard: this.props.playerCards[0],
+                    palyerTwoCard: this.props.playerCards[1]
+                });
+            }else if(res === 2){
+                this.setState({
+                    palyerOneCard: this.props.playerCards[1],
+                    palyerTwoCard: this.props.playerCards[0]
+                });
+            }
+        })
     }
     render() {
         return (
             <Grid>
                 <Grid.Row>
-                    <Card.Description className="ur-card">MY CARD</Card.Description>
+                    <Card.Description className="ur-card">
+                    OTHER PLAYER CARD: {this.state.palyerTwoCard}
+                    </Card.Description>
+                </Grid.Row>
+                <Grid.Row>
+                    <Card.Description className="ur-card">
+                    MY CARD: {this.state.palyerOneCard}
+                    </Card.Description>
                 </Grid.Row>
             </Grid>
         );
